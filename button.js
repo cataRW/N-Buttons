8377d105aaa00ced4e251a7020fed50807d8f9d4
cataRW <86371141+cataRW@users.noreply.github.com>
2021-06-27 00:16:22 +0300

    Add files via upload

---
+++ button.js
@@ -0,0 +1,34 @@
+function randomNumb(number) {
+  return Math.floor((Math.random() * number) + 1);
+}
+
+function numberCheck() {
+  const inputBox = document.getElementById('inputBox');
+  let randomNumber = randomNumb(inputBox.value);
+
+  if (isNaN(inputBox.value)) {
+    document.querySelector('.status').textContent = "!!!You must insert a number!!!";
+  }else {
+    for(let i = 1; i <= inputBox.value; ++i) {
+      $('.buttons').append(`<button type="button" id="` + i + `" onclick="choiceCheck(this.id)" class ="miniButtons" >Button ` + i + `</button>`);
+    }
+    document.querySelector('.status').textContent = " ";
+    document.getElementById('submit').style.visibility = 'hidden';
+    document.getElementById('restart').style.visibility = 'visible';
+  }
+}
+
+
+function choiceCheck(theChoosenButton) {
+  if (theChoosenButton === randomNumb) {
+    document.querySelector('.status').textContent = "!!!CONGRATS, you win amigo!!!";
+    document.querySelector('.status').style.backgroundColor = "green";
+  } else {
+    document.querySelector('.status').textContent = "Almost.. but you lost..";
+    document.querySelector('.status').style.backgroundColor = "red";
+  }
+}
+
+function restart() {
+  window.location.reload(true);
+}
